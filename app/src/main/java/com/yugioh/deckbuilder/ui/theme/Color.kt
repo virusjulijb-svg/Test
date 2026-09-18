ff2691188671d091513900bd25d9442e64d29838
virusjulijb-svg <virusjuli.jb@gmail.com>
2026-09-19 01:39:58 +0200

    Remove app files from main (empty base branch)

--- app/src/main/java/com/yugioh/deckbuilder/ui/theme/Color.kt
+++
@@ -1,9 +0,0 @@
-package com.yugioh.deckbuilder.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val GoldPrimary = Color(0xFFC9A227)
-val DarkSurface = Color(0xFF1B1B1F)
-val DarkBackground = Color(0xFF121214)
-val ErrorRed = Color(0xFFCF6679)
-val SuccessGreen = Color(0xFF4CAF50)
